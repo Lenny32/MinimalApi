883937954dc2ce35bdb9ba706ce3ab7f8245d9cc
Lenny32 <lenaic.cormier@gmail.com>
2022-01-02 22:26:50 +0100

    fix

--- MinimalApiRegister/RegisterMinimalApi.cs
+++ MinimalApiRegister/RegisterMinimalApi.cs
@@ -42,7 +42,7 @@ namespace Microsoft.AspNetCore.Builder
         }
 
         private static List<Assembly> _assemblies { get; set; }
-        public static Assembly[] Assemblies => Assemblies.ToArray();
+        public static Assembly[] Assemblies => _assemblies.ToArray();
         public static void MapMinimalApi(this IEndpointRouteBuilder app)
         {
             if (!_assemblies.Any())
