3ad654075e5ba5c7949a775abd151d278eed8992
Lenny32 <lenaic.cormier@gmail.com>
2022-01-02 21:17:25 +0100

    improvments to registration

--- MinimalApiRegister/RegisterMinimalApi.cs
+++ MinimalApiRegister/RegisterMinimalApi.cs
@@ -1,15 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Microsoft.AspNetCore.Builder
 {
     public static class RegisterMinimalApi
     {
+        static RegisterMinimalApi()
+        {
+            Assemblies = new List<Assembly?>() 
+            { 
+                Assembly.GetEntryAssembly() 
+            };
+        }
+
+        public static IServiceCollection AddEndpoints(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+        {
+            if(!Assemblies.Contains(assembly))
+                Assemblies.Add(assembly);
+            return services;
+        }
+
+
+        public static List<Assembly?> Assemblies { get; internal set; }
         public static void MapMinimalApi(this IEndpointRouteBuilder app)
         {
-            var types = Assembly.GetExecutingAssembly().GetTypes()
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            var types = Assemblies.Where(x => x != null).SelectMany(x => x.GetTypes())
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
                 .Where(x => typeof(IEndPointApi).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
                 .ToList();
+
+
             foreach (var type in types)
             {
                 (Activator.CreateInstance(type) as IEndPointApi)?.Register(app);
--- MinimalApiRegister/Usings.cs
+++ MinimalApiRegister/Usings.cs
@@ -1,3 +1,6 @@
-﻿global using Microsoft.AspNetCore.Http;
+﻿global using System;
+global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Routing;
 global using Microsoft.AspNetCore.Builder;
+global using System.Linq;
+global using System.Collections.Generic;
