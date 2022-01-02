dd647993b2623762780090b02eb745b52dd0f551
Lenny32 <lenaic.cormier@gmail.com>
2022-01-02 21:57:47 +0100

    Fixes

--- MinimalApiRegister/RegisterMinimalApi.cs
+++ MinimalApiRegister/RegisterMinimalApi.cs
@@ -8,37 +8,55 @@ namespace Microsoft.AspNetCore.Builder
     {
         static RegisterMinimalApi()
         {
-            Assemblies = new List<Assembly?>() 
-            { 
-                Assembly.GetEntryAssembly() 
-            };
+            _assemblies = new List<Assembly>();
         }
 
+        /// <summary>
+        /// Register the entry point as source of the endpoint. 
+        /// Microsoft.AspNetCore.Mvc.IEndPointApi
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public static IServiceCollection AddEndpoints(this IServiceCollection services)
         {
-            return services;
+#pragma warning disable CS8604 // Possible null reference argument.
+            return AddEndpoints(services, Assembly.GetEntryAssembly());
+#pragma warning restore CS8604 // Possible null reference argument.
         }
+
         public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
         {
-            if(!Assemblies.Contains(assembly))
-                Assemblies.Add(assembly);
+            return AddEndpoints(services, assembly);
+        }
+
+        public static IServiceCollection AddEndpoints(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            if (assemblies == null)
+                throw new ArgumentNullException($"The parameter {nameof(assemblies)} cannot be null.");
+            foreach (var assembly in assemblies)
+            {
+                if (!_assemblies.Contains(assembly))
+                    _assemblies.Add(assembly);
+            }
             return services;
         }
 
-
-        public static List<Assembly?> Assemblies { get; internal set; }
+        private static List<Assembly> _assemblies { get; set; }
+        public static Assembly[] Assemblies => Assemblies.ToArray();
         public static void MapMinimalApi(this IEndpointRouteBuilder app)
         {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            var types = Assemblies.Where(x => x != null).SelectMany(x => x.GetTypes())
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            if (!_assemblies.Any())
+                throw new InvalidOperationException("Unable to find the required services. Please add all the required services by calling 'IServiceCollection.AddEndpoints' with or without extra parameters inside the call to 'ConfigureServices(...)' in the application startup code.");
+
+            var types = _assemblies.Where(x => x != null).SelectMany(x => x.GetTypes())
                 .Where(x => typeof(IEndPointApi).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
                 .ToList();
 
-
             foreach (var type in types)
             {
-                (Activator.CreateInstance(type) as IEndPointApi)?.Register(app);
+                (app.ServiceProvider.GetService(type) as IEndPointApi)?.Register(app);
+
+                //(Activator.CreateInstance(type) as IEndPointApi)?.Register(app);
             }
         }
     }
